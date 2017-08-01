102ad28d1ba6e5e7977a350bfc85a3dab49a1b45
Hakan Karadis <hakan@koding.com>
2017-07-31 17:21:13 -0700

    rename public folder

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
+app.use(express.static('docs'));
 
 app.listen(8090, function() {
   console.log('Server is up and running!');
